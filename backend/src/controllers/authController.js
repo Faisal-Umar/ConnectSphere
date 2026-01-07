e3bf025974dcc1c585a575992d177b53aa139f1c
Faisal Umar <faisalumar0484@gmail.com>
2026-01-07 23:29:20 +0530

    Login API with JWT

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 
 exports.signup = async (req, res) => {
   try {
@@ -31,6 +33,44 @@ exports.signup = async (req, res) => {
         email: user.email
       }
     });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email and password are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    });
+
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
